22294b09dcbb35f99a22e4f660c0bfc0c11461a7
kaushalgabani <kaushalgabani21@gmail.com>
2023-09-20 23:13:36 -0400

    Add more blank pages with smooth scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+declare var $: any; // Declare jQuery
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'portfolio';
+  
 }
+
