ee2eea2bc6b8c00f1c7f4430a17fdef44dc3f856
kaushalgabani <kaushalgabani21@gmail.com>
2023-09-26 22:37:06 -0400

    fix toggle button flow and change image path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
-declare var $: any; // Declare jQuery
+// declare var $: any; // Declare jQuery
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'portfolio';
-  
-}
 
+  ngOnInit(): void {
+    //Menu toogle
+    $(document).ready(function () {
+      const $toggleCheckbox = $('.toggler');
+      const $menu = $('.menu');
+
+      $toggleCheckbox.on('change', function () {
+        if ($(this).is(':checked')) {
+          $menu.addClass('active');
+        } else {
+          $menu.removeClass('active');
+        }
+      });
+
+      $('.menu ul li a').on('click', function () {
+        $toggleCheckbox.prop('checked', false);
+        $menu.removeClass('active');
+      });
+
+      // Smooth Scrolling
+      $(document).ready(function () {
+        $('a[href^="#"]').on('click', function (e) {
+          e.preventDefault();
+          var target = (this as HTMLAnchorElement).hash;
+          var $target = $(target);
+          console.log(target);
+
+          $('html, body').animate({
+            scrollTop: $target.offset()!.top
+          }, 900);
+        });
+      });
+    });
+  }
+}
